b0c5ae898b248a9ce125d6dc615f155140038e54
MaRIM41 <109277581+MaRIM41@users.noreply.github.com>
2022-07-15 09:51:21 +0200

    Update Service.js

--- components/Service.js
+++ components/Service.js
@@ -3,7 +3,7 @@ import styles from "../styles/components/Service.module.css";
 export default function Service({ imgURL, heading, details, theme }) {
   return (
     <div className={theme == "red" ? styles.redContainer : styles.whiteContainer}>
-      <img src={imgURL} alt="service" className={`${styles.img} service_img`} width="75px" />
+      <img src={imgURL} alt="service" className={`${styles.img} service_img`} width="250px" />
       <h1 className={styles.title}>{heading}</h1>
       <p className={styles.details}>{details}</p>
     </div>
