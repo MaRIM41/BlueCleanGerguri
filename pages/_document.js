610350fcdb6c37b56890452e88003dc6dbe2f273
MaRIM41 <109277581+MaRIM41@users.noreply.github.com>
2022-07-15 14:45:45 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -9,14 +9,14 @@ class MyDocument extends Document {
         <Head>
 
           {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script async src={`https://www.googletagmanager.com/gtag/js?id=G-NT9325229F`} />
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=G-GV83SQ054R`} />
           <script
             dangerouslySetInnerHTML={{
               __html:` window.dataLayer = window.dataLayer || [];
                         function gtag(){dataLayer.push(arguments);}
                         gtag('js', new Date());
                         
-                        gtag('config', 'G-NT9325229F');
+                        gtag('config', 'G-GV83SQ054R');
                     `,
             }}
           />
