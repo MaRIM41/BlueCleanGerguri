dbdfd93b8eebf917f462df57f097fe05d60efe8b
MaRIM41 <109277581+MaRIM41@users.noreply.github.com>
2022-07-15 14:07:42 +0200

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -121,6 +121,7 @@ export default function NavBar() {
                 icon={faEnvelope}
                 className={styles.navbar__mapLocIcon}
               />
+            mailen
             </a>
             )}      
         </div>
