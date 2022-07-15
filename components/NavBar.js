5f148b784472ed03c6e73c1a696e8b43a884eeb8
MaRIM41 <109277581+MaRIM41@users.noreply.github.com>
2022-07-15 14:02:59 +0200

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -11,7 +11,7 @@ import {
   faMapLocationDot,
   faPhone,
   faEnvelope,
-  faWhatsappSquare,
+  faMessageSmile,
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function NavBar() {
@@ -108,7 +108,7 @@ export default function NavBar() {
               rel="noreferrer"
             >
               <FontAwesomeIcon
-                icon={faPhone}
+                icon={faMessageSmile}
                 className={styles.navbar__mapLocIcon}
               />
             </a>
